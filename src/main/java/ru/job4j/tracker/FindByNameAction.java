c05eb4c114be8ea053061070c2ba0d2dffcfa512
Igor M <wismu@yandex.ru>
2022-12-19 22:10:36 +0300

    12.1 Зависимость от System.out [#33568]

--- src/main/java/ru/job4j/tracker/FindByNameAction.java
+++ src/main/java/ru/job4j/tracker/FindByNameAction.java
@@ -6,6 +6,7 @@ public class FindByNameAction implements UserAction {
     public FindByNameAction(Output out) {
         this.out = out;
     }
+
     @Override
     public String name() {
         return "Find items by name";
