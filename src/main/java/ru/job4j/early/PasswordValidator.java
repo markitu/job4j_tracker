581777e660746c0b969f932ef82040750e84354d
Igor M <wismu@yandex.ru>
2022-12-27 12:34:14 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #375006]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -2,77 +2,50 @@ package ru.job4j.early;
 
 public class PasswordValidator {
     public static String validate(String password) {
+        boolean upper = false;
+        boolean lower = false;
+        boolean digit = false;
+        boolean special = false;
+
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
         }
         if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
-        if (!isStringContainsUpperCase(password)) {
+        for (char c : password.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                upper = true;
+            }
+            if (Character.isLowerCase(c)) {
+                lower = true;
+            }
+            if (Character.isDigit(c)) {
+                digit = true;
+            }
+            if (!Character.isLetter(c) && !Character.isDigit(c)) {
+                special = true;
+            }
+            if (upper && lower && digit && special) {
+                break;
+            }
+        }
+        if (!upper) {
             throw new IllegalArgumentException("Password should contain at least one uppercase letter");
         }
-        if (!isStringContainsLowerCase(password)) {
+        if (!lower) {
             throw new IllegalArgumentException("Password should contain at least one lowercase letter");
         }
-        if (!isStringContainsDigit(password)) {
+        if (!digit) {
             throw new IllegalArgumentException("Password should contain at least one figure");
         }
-        if (!isStringContainsSpecialSymbol(password)) {
+        if (!special) {
             throw new IllegalArgumentException("Password should contain at least one special symbol");
         }
         if (isPasswordContainsInvalidSubString(password)) {
             throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
         }
         return password;
-    }
-
-    private static boolean isStringContainsUpperCase(String string) {
-        boolean result = false;
-        for (int i = 0; i < string.length(); i++) {
-            if (Character.isUpperCase(string.charAt(i))) {
-                result = true;
-                break;
-            }
-        }
-        return result;
-    }
-
-    private static boolean isStringContainsLowerCase(String string) {
-        boolean result = false;
-        for (int i = 0; i < string.length(); i++) {
-            if (Character.isLowerCase(string.charAt(i))) {
-                result = true;
-                break;
-            }
-        }
-        return result;
-    }
-
-    private static boolean isStringContainsDigit(String string) {
-        boolean result = false;
-        for (int i = 0; i < string.length(); i++) {
-            if (Character.isDigit(string.charAt(i))) {
-                result = true;
-                break;
-            }
-        }
-        return result;
-    }
-
-    private static boolean isStringContainsSpecialSymbol(String string) {
-        boolean result = false;
-        for (int i = 0; i < string.length(); i++) {
-            int code = string.codePointAt(i);
-            if (isSpecialSymbol(code)) {
-                result = true;
-                break;
-            }
-        }
-        return result;
-    }
-
-    private static boolean isSpecialSymbol(int code) {
-        return code >= 32 && code <= 47;
     }
 
     private static boolean isPasswordContainsInvalidSubString(String password) {
