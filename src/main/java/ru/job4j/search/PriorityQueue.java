7568da506aac7064f3c3f9604e33c09359f1c9ac
Igor M <wismu@yandex.ru>
2023-01-07 10:44:08 +0300

    5. Очередь с приоритетом на LinkedList [#41670 #378374]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -8,12 +8,11 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (element.getPriority() <= task.getPriority()) {
-                index++;
-            } else {
+            if (task.getPriority() < element.getPriority()) {
                 tasks.add(index, task);
                 break;
             }
+            index++;
         }
         this.tasks.add(index, task);
     }
