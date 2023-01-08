e23d1f346f775a3486d2b6af3539687bdd32fae5
Igor M <wismu@yandex.ru>
2023-01-08 18:52:09 +0300

    1. Интерфейс Queue [#504942 #378863]

--- src/main/java/ru/job4j/queue/AppleStore.java
+++ src/main/java/ru/job4j/queue/AppleStore.java
@@ -1,5 +1,6 @@
 package ru.job4j.queue;
 
+import java.util.NoSuchElementException;
 import java.util.Queue;
 
 public class AppleStore {
@@ -7,6 +8,12 @@ public class AppleStore {
     private final int count;
 
     public AppleStore(Queue<Customer> queue, int count) {
+        if (queue == null) {
+            throw new IllegalArgumentException("queue cannot be null");
+        }
+        if (count < 0) {
+            throw new IllegalArgumentException("count cannot be less than 0");
+        }
         this.queue = queue;
         this.count = count;
     }
@@ -15,12 +22,18 @@ public class AppleStore {
         for (int i = 1; i < count; i++) {
             queue.poll();
         }
+        if (queue.peek() == null) {
+            throw new NoSuchElementException("There are no happy customers in the queue.");
+        }
         return queue.peek().name();
     }
 
     public String getLastUpsetCustomer() {
         for (int i = 1; i <= count; i++) {
             queue.poll();
+        }
+        if (queue.peek() == null) {
+            throw new NoSuchElementException("There are no upset customers in the queue.");
         }
         return queue.peek().name();
     }
