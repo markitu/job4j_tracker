969956e7a3cdf4410ab88f8df49f32fb9a0b7b31
Igor M <wismu@yandex.ru>
2023-01-08 10:43:02 +0300

    9. Правила Checkstyle [#504981 #378560]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -8,13 +8,13 @@ public class Broken {
 
     Broken() { }
 
-    void echo() { }
+    public void echo() { }
 
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) { }
+    public void method(int a, int b, int c, int d, int e, int f, int g) { }
 }
