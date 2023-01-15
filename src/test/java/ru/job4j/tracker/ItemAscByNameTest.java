7b29f2656d25b8da21ab65b80c594ce3dd8e94a1
Igor M <wismu@yandex.ru>
2023-01-15 14:35:58 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemAscByNameTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ItemAscByNameTest {
+    @Test
+    void compareItemAscByName() {
+        List<Item> items = new ArrayList<>();
+        List<Item> expected = new ArrayList<>();
+        items.add(new Item("Zahar"));
+        items.add(new Item("Nikolay"));
+        items.add(new Item("Anatoliy"));
+        items.add(new Item("Igor"));
+        expected.add(new Item("Anatoliy"));
+        expected.add(new Item("Igor"));
+        expected.add(new Item("Nikolay"));
+        expected.add(new Item("Zahar"));
+        items.sort(new ItemAscByName());
+        assertThat(items).isEqualTo(expected);
+    }
+}
