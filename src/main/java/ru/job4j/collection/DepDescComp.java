fa8008d4487965e24fbf15dba67fa7015da8e882
Igor M <wismu@yandex.ru>
2023-01-21 08:56:20 +0300

    2. Отсортировать департаменты [#34352 #383285]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,9 +5,9 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        String firstEl1 = o1.split("/")[0];
-        String firstEl2 = o2.split("/")[0];
-        int firstElComp = String.CASE_INSENSITIVE_ORDER.compare(firstEl2, firstEl1);
-        return firstElComp != 0 ? firstElComp : String.CASE_INSENSITIVE_ORDER.compare(o1, o2);
+        String left = o1.split("/")[0];
+        String right = o2.split("/")[0];
+        int result = String.CASE_INSENSITIVE_ORDER.compare(right, left);
+        return result != 0 ? result : String.CASE_INSENSITIVE_ORDER.compare(o1, o2);
     }
 }
