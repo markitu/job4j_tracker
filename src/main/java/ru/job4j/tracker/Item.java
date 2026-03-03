f38ff37292ce26517df30376cc6ac1ddfa03d838
markitu <40546844+markitu@users.noreply.github.com>
2026-03-03 19:48:52 +0300

    Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,19 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
     public int getId() {
         return id;
     }
@@ -19,4 +32,5 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
 }
