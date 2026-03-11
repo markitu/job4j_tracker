a9f577b1a7221a1eb391b595dce3ebcae52fa818
markitu <40546844+markitu@users.noreply.github.com>
2026-03-11 11:34:46 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,17 +1,20 @@
 package ru.job4j.tracker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
 
-    public Item addItem(Item item) {
+    public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
         return item;
     }
 
-    public Item findItemById(int id) {
+    public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
@@ -21,5 +24,25 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        List<Item> result = new ArrayList<>();
+        for (Item item : items) {
+            if (item != null) {
+                result.add(item);
+            }
+        }
+        return result.toArray(new Item[0]);
+    }
+
+    public Item[] findByName(String name) {
+        List<Item> result = new ArrayList<>();
+        for (Item item : items) {
+            if (item != null && name.equals(item.getName())) {
+                result.add(item);
+            }
+        }
+        return result.toArray(new Item[0]);
     }
 }
