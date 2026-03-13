bb65f95b51a63f0713a23a8730492cb363d41d32
markitu <40546844+markitu@users.noreply.github.com>
2026-03-13 14:12:26 +0300

    5. Tracker - хранилище [#396 #559771]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -24,9 +24,9 @@ public class Tracker {
 
     public Item[] findByName(String key) {
         Item[] items = findAll();
-        Item[] result = new Item[items.length];
+        Item[] result = new Item[size];
         int count = 0;
-        for (int i = 0; i < findAll().length; i++) {
+        for (int i = 0; i < size; i++) {
             if (key.equals(items[i].getName())) {
                 result[count] = items[i];
                 count++;
