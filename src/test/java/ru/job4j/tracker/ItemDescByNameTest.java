eccc8f2bc30293fc826366140832770e263780b8
markitu <40546844+markitu@users.noreply.github.com>
2026-04-16 21:43:49 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemDescByNameTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ItemDescByNameTest {
+    @Test
+    void compareItemDescByName() {
+        List<Item> items = new ArrayList<>();
+        List<Item> expected = new ArrayList<>();
+        items.add(new Item("Zahar"));
+        items.add(new Item("Nikolay"));
+        items.add(new Item("Anatoliy"));
+        items.add(new Item("Igor"));
+        expected.add(new Item("Zahar"));
+        expected.add(new Item("Nikolay"));
+        expected.add(new Item("Igor"));
+        expected.add(new Item("Anatoliy"));
+        items.sort(new ItemDescByName());
+        assertThat(items).isEqualTo(expected);
+    }
+}
