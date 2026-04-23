788f10f63b188d77b5b9beb7d2d0735416578b88
markitu <40546844+markitu@users.noreply.github.com>
2026-04-23 17:41:03 +0300

    3. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,26 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> departments) {
+        Set<String> temp = new LinkedHashSet<>();
+        for (String value : departments) {
+            String start = "";
+            for (String el : value.split("/")) {
+                start += start.length() > 0 ? "/" + el : el;
+                temp.add(start);
+            }
+        }
+        return new ArrayList<>(temp);
+    }
+
+    public static void sortAsc(List<String> departments) {
+        departments.sort(Comparator.naturalOrder());
+    }
+
+    public static void sortDesc(List<String> departments) {
+        Collections.sort(departments, new DepartmentsDescComparator());
+    }
+}
---
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepartmentsDescComparator implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        String left = o1.split("/")[0];
+        String right = o2.split("/")[0];
+        int result = String.CASE_INSENSITIVE_ORDER.compare(right, left);
+        return result != 0 ? result : String.CASE_INSENSITIVE_ORDER.compare(o1, o2);
+    }
+}
